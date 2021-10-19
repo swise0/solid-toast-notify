8bc93b606178bb3f1d0c90bcbf7512ca4ef8aafe
Swise-Haymann <swise191@gmail.com>
2021-10-19 18:53:22 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ function App() {
   return (
     <>
       <div class="trigger">
-      <button id="success" onclick={()=>notify({in:'row success intro',duration:2999,out:'outro'})}>Success</button>
-        <button id="error" onclick={()=>notify({in:'row error intro',out:'outro',progressbar:'notif-progress-bar',duration:100000})}>Error</button>
-        <button id="alert" onclick={()=>notify({in:'row alert intro',duration:2000})}>Alert</button>
-        <button id="info" onclick={()=>notify({in:'row info intro',duration:1040})}>Info</button>
-        <button id="custom" onclick={()=>notify({in:'custom intro',out:'outro',block:'<a href=\'www.href.com\'>Click Here</a>'})}>Custom</button>
-  <button id="custom" onclick={()=>notify({in:'row custom'})}>Custom</button>
+        <button id="success" onclick={() => notify({ in: 'row success intro', duration: 2.9, progressbar: 'notif-progress-bar', out: 'outro' })}>Success (2.9s)</button>
+        <button id="error" onclick={() => notify({ in: 'row error intro', out: 'outro', progressbar: 'notif-progress-bar', duration: 10})}>Error (10s)</button>
+        <button id="alert" onclick={() => notify({ in: 'row alert intro', duration: 6, out: 'outro',progressbar: 'notif-progress-bar' })}>Alert (6s)</button>
+        <button id="info" onclick={() => notify({ in: 'row info intro', duration: 1.04, progressbar: 'notif-progress-bar',out: 'outro' })}>Info (1.04s)</button>
+        <button id="custom" onclick={() => notify({out: 'outro', block: '<a href=\'www.href.com\'>Click Here</a>',in: 'row custom intro' })}>Custom</button>
+        <button id="custom" onclick={() => notify({ in: 'row custom' })}>Custom</button>
       </div>
+
       <script src='mynotif.js'></script>
     </>);
 }
--- src/notif.js
+++ src/notif.js
@@ -1,14 +1,14 @@
 let container = document.createElement("div");
 container.id = "container";
 let id = 0;
-export default function notify(opts = {}) {
-    let position = 'bottomright';//but you can choose bottomleft or topright or bottomright
+export default function notify(o = {}) {
+    let position = 'bottomleft'//but you can choose bottomleft or topright or bottomright
     switch (position) {
         case 'topright':
             position = 'top:0;right:0'
             break;
         case 'bottomleft':
-            position = 'bottom:0';
+            position = 'bottom:0'
             break;
         case 'bottomright':
             position = 'bottom:0;right:0'
@@ -18,31 +18,45 @@ export default function notify(opts = {}) {
     }
     container.style = position;
     if (!container.children[0]) { let body = document.getElementsByTagName('body')[0]; body.append(container); }
-    let duration = opts.duration || 0;
-    let i = (opts.in || '').match(/[^ ,]+/g)
-    let o = (opts.out || '').match(/[^ ,]+/g)
-    let p = (opts.progressbar||'').match(/[^ ,]+/g)
+    let d = o.duration * 1000 || 0;
+    let a = (o.in || '').match(/[^ ,]+/g)
+    let b = (o.out || '').match(/[^ ,]+/g)
+    let p = (o.progressbar || '').match(/[^ ,]+/g)
     id++;
-    let row = document.createElement('div');
-    let progressbar = duration && p ? "<div class='" + p.join(' ') + "'></div>" : "";
-    row.innerHTML = opts.block || "Box </br><a onclick='()=>body.style=\''fontsize:5000'\'>Close</a>" + id + progressbar;
-    i && row.classList.add(...i);
+    let r = document.createElement('div');
+    // let clear = document.createElement('button')
+    // clear.innerHTML = 'Clear Notifications';
+    // clear.addEventListener('click', () => clearf());
+    // let c = (opts.clearbtn || '').match(/[^ ,]+/g)
+    // clear.classList.add(...c)
+    let close = e => {
+        b && e.classList.add(...b)
+        setTimeout(() => { e.remove(); !container.children[0] && container.remove() }, parseFloat(getComputedStyle(r).getPropertyValue("animation-duration")) * 1000)
+    }
+    // let clearf = () => {
+    //     container.childNodes.forEach(v=>{cleartInterval();v.remove()})
+    //     console.log(container.children)}
+    let progressbar = d && p ? "<div class='" + p.join(' ') + "'></div>" : "";
+    r.innerHTML = o.block || "Box </br><a onclick='()=>body.style=\''fontsize:5000'\'>Close</a>" + id + progressbar;
+    a && r.classList.add(...a);
     if (position.match(/top/i)) {
-        container.append(row);
+        container.append(r)
         container.scrollTo(0, container.scrollHeight)
     }
     else {
-        container.prepend(row);
+        container.prepend(r)
         container.scrollTo(container.scrollHeight, 0)
     }
-    let s = 0, a = setInterval(() => {
-        s += 100;
-        if (p && duration) { row.querySelector('.' + p[0]).style.width = (duration - s) / duration * 100 + '%'; }
-        if (s >= duration) { clearInterval(a); duration && close(row); }
-    }, 100);
+    // if (container.children.length == 2 && container.children.length < 3) { container.append(clear) }
 
-    let close = (e) => {
-        o && e.classList.add(...o);
-        setTimeout(() => { e.remove();!container.children[0] && container.remove() }, parseFloat(getComputedStyle(row).getPropertyValue("animation-duration")) * 1000)
-    }
+    // container.scrollHeight > window.,innerHeight;
+
+
+    let s = 0, t = setInterval(() => {
+        s += 16.66
+        if (p && d) r.querySelector('.' + p[0]).style.width = (d - s) / d * 100 + '%'
+        if (s >= d) { clearInterval(t); d && close(r) }
+    });
+
+
 }
